28407e921aef6c69e527e07b930527df8332ebe3
Miraiz <makhmudjonov14@gmail.com>
2025-07-24 16:06:57 +0500

    fix

--- apps/full_score/view.py
+++ apps/full_score/view.py
@@ -1,16 +1,20 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAdminUser
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 
-from apps.models import Student
-from apps.serializers import StudentCombinedScoreSerializer
+from .models import Student
+from .serializers import StudentCombinedScoreSerializer
+from .pagination import StandardResultsSetPagination  # agar siz custom pagination class ishlatsangiz
 
 class StudentScoreView(ListAPIView):
-    queryset = Student.objects.prefetch_related('applications__items__score', 'gpa_records', 'faculty', 'level')
+    queryset = Student.objects.prefetch_related(
+        'applications__items__score', 'gpa_records', 'faculty', 'level'
+    )
     serializer_class = StudentCombinedScoreSerializer
     permission_classes = [IsAdminUser]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['faculty', 'level']  # filter by faculty/course
     search_fields = ['full_name', 'group']
     ordering_fields = ['gpaball', 'score_total', 'total_score']
+    pagination_class = StandardResultsSetPagination  # yoki DRF default ishlatiladi
--- core/settings.py
+++ core/settings.py
@@ -169,6 +169,11 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 20,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
 
 # Swagger sozlamasi
