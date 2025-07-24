a3ef8c80ad4512a4947184bd60f8bc82fb5e9e1a
Makhmudjonov <makhmudjonov14@gmail.com>
2025-07-24 23:32:57 +0500

    fix

--- core/settings.py
+++ core/settings.py
@@ -144,11 +144,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tashkent'
+USE_TZ = True
 
 USE_I18N = True
 
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
