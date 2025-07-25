910fde4f1d0434d142902141eb6bd3d4a87a198b
Miraiz <makhmudjonov14@gmail.com>
2025-07-25 18:44:38 +0500

    fix

--- apps/file_download.py
+++ apps/file_download.py
@@ -1,14 +1,21 @@
 # apps/views.py yoki boshqa tegishli views fayliga qo‘ying
-from django.http import FileResponse, HttpResponseNotFound
+# views.py
+from django.http import FileResponse, Http404
 from django.conf import settings
 import os
 from urllib.parse import unquote
 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
 def download_file(request, path):
-    decoded_path = unquote(path)  # bu yer muhim
-    file_path = os.path.join(settings.MEDIA_ROOT, path)
-    if os.path.exists(file_path):
-        response = FileResponse(open(file_path, 'rb'))
-        response['Content-Disposition'] = f'attachment; filename="{os.path.basename(file_path)}"'
-        return response
-    return HttpResponseNotFound("Fayl topilmadi.")
+    decoded_path = unquote(path)  # <<< bu MUHIM
+    file_path = os.path.join(settings.MEDIA_ROOT, decoded_path)
+
+    if not os.path.exists(file_path):
+        raise Http404("File not found")
+
+    return FileResponse(open(file_path, 'rb'), as_attachment=True)
+
