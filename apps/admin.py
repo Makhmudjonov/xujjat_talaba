1bf8eae55304755f2015f0a97df921f38ff4560b
Miraiz <makhmudjonov14@gmail.com>
2025-07-26 19:28:23 +0500

    fix

--- apps/admin.py
+++ apps/admin.py
@@ -116,7 +116,7 @@ class SpecialApplicationStudentAdmin(SimpleHistoryAdmin):
 
 @admin.register(ApplicationItem)
 class ApplicationItemAdmin(SimpleHistoryAdmin):
-    list_display = ("id", "get_student_name", "get_level", "direction", "application_file")
+    list_display = ("id", "get_student_name", "get_level", "direction", "file")
     search_fields = ('application__student__full_name',)
 
     
