e804f90cbfc14c196d0d405ef3f75e5cf9001613
Makhmudjonov <makhmudjonov14@gmail.com>
2025-07-27 21:56:36 +0500

    fix

--- apps/api/speciality/stom.py
+++ apps/api/speciality/stom.py
@@ -13,7 +13,7 @@ class SyncStudentDataStomAPIView(APIView):
     permission_classes = [IsAdminUser]
 
     def get(self, request):
-        students = Student.objects.filter(student_id_number__startswith='366')
+        students = Student.objects.filter(student_id_number__startswith='3661')
         updated = 0
         skipped = 0
         errors = []
--- apps/api/speciality/tma.py
+++ apps/api/speciality/tma.py
@@ -13,7 +13,7 @@ class SyncStudentDataTmaAPIView(APIView):
     permission_classes = [IsAdminUser]
 
     def get(self, request):
-        students = Student.objects.filter(student_id_number__startswith='364')
+        students = Student.objects.filter(student_id_number__startswith='3641')
         updated = 0
         skipped = 0
         errors = []
