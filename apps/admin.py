dd39991f1b667ec2014c861ccfe95f23b64ca858
Makhmudjonov <makhmudjonov14@gmail.com>
2025-07-28 00:02:57 +0500

    fix

--- apps/admin.py
+++ apps/admin.py
@@ -96,7 +96,7 @@ class ScoreAdmin(SimpleHistoryAdmin):
 @admin.register(Application)
 class ApplicationAdmin(SimpleHistoryAdmin):
     list_display = ('student', 'application_type', 'status', 'submitted_at', 'student__university','student__university1', 'student__faculty', 'student__level')
-    list_filter = ('application_type','student__university', 'student__university1', 'student__faculty','student__speciality', 'student__hemis_group',DuplicateApplicationFilter)
+    list_filter = ('application_type','student__university', 'student__university1', 'student__faculty','student__specialty', 'student__group_hemis',DuplicateApplicationFilter)
     search_fields = ('student__full_name', 'student__student_id_number')  # misol uchun
     actions = ['export_as_excel']
     
@@ -141,8 +141,8 @@ class ApplicationAdmin(SimpleHistoryAdmin):
                 student.university1.name if student.university1 else "",
                 student.faculty.name if student.faculty else "",
                 student.specialty.name if student.specialty else "",
-                student.hemis_group.name if student.hemis_group else "",
-                student.hemis_group.lang if student.hemis_group else "",
+                student.group_hemis.name if student.group_hemis else "",
+                student.group_hemis.lang if student.group_hemis else "",
                 student.level.name if student.level else "",
                 student.group if student.group else "",
                 str(app.application_type),
