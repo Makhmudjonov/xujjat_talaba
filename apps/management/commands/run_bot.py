8adca72cb1f6a3d1876bcc58a67467456139c4eb
Miraiz <makhmudjonov14@gmail.com>
2025-07-31 11:15:23 +0500

    fix

--- apps/management/commands/run_bot.py
+++ apps/management/commands/run_bot.py
@@ -1,10 +1,10 @@
 # apps/management/commands/run_bot.py
 
 from django.core.management.base import BaseCommand
-from apps.bot import run_bot  # bot boshlovchi funksiyangiz shu bo‘lsa
+from apps.bot.bot import start_bot  # bot boshlovchi funksiyangiz shu bo‘lsa
 
 class Command(BaseCommand):
     help = 'Telegram botni ishga tushuradi'
 
     def handle(self, *args, **kwargs):
-        run_bot()  # bu yerda botni ishga tushuruvchi funksiya bo‘lishi kerak
+        start_bot()  # bu yerda botni ishga tushuruvchi funksiya bo‘lishi kerak
