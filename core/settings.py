036a8110a7c89101565eb9315f8507334c0544a4
Miraiz <makhmudjonov14@gmail.com>
2025-07-31 12:22:35 +0500

    fix

--- core/settings.py
+++ core/settings.py
@@ -113,25 +113,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tanlov',
-        'USER': 'miraziz',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432', # PostgreSQL uchun standart port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'tanlov',
+#         'USER': 'miraziz',
+#         'PASSWORD': '123',
+#         'HOST': 'localhost',
+#         'PORT': '5432', # PostgreSQL uchun standart port
+#     }
+# }
 
 
 # Password validation
