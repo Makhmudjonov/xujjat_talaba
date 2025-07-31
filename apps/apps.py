dba9bb4ed64d3c662c83c3a2b3e5cdf4ed53bb81
Miraiz <makhmudjonov14@gmail.com>
2025-07-31 10:53:53 +0500

    fix

--- apps/apps.py
+++ apps/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
+import threading
 
 class AppsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps'
+
+    def ready(self):
+        import sys
+        if 'runserver' not in sys.argv:
+            return
+
+        from . import bot  # bot.py faylingiz
+        threading.Thread(target=bot.run, daemon=True).start()
