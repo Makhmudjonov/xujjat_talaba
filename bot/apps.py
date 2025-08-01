7581ffa520771bca1829d900efd66c798d636713
Miraiz <makhmudjonov14@gmail.com>
2025-08-01 11:13:19 +0500

    fix

--- bot/apps.py
+++ bot/apps.py
@@ -8,9 +8,9 @@ class BotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bot'
 
-    # def ready(self):
-    #     if os.environ.get("RUN_MAIN") != "true":
-    #         loop = asyncio.new_event_loop()
-    #         asyncio.set_event_loop(loop)
-    #         loop.create_task(run_bot())
-    #         loop.run_forever()
+    def ready(self):
+        if os.environ.get("RUN_MAIN") != "true":
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            loop.create_task(run_bot())
+            loop.run_forever()
